24c4eedd93259a15d95b6c7fc4e18290a1c7d87d
Sparsh2509 <sparshgupta250904@gmail.com>
2025-08-02 20:18:12 +0530

    Using grid search cv for more precise

--- Heart_Disease_Predict_Model.py
+++ Heart_Disease_Predict_Model.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split , GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score , classification_report, confusion_matrix
